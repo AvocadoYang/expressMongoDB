a810e214b477ededea7b2b934d2f048659bc3375
Avocado <cl4m3861026@gmail.com>
2023-04-25 09:08:49 +0800

    Problem

--- controllers/user.js
+++ controllers/user.js
@@ -1,84 +1,74 @@
 const User = require('../models/user');
 const header = require('../header');
-<<<<<<< HEAD
-getUserInfo = async (req, res) =>{
-    await User.find();
-}
-=======
 const successHandle = require("../service/successHandle");
 const customiError = require("../service/customiError");
 const validator = require("validator");
->>>>>>> ca8c5bfd6c602d60ceecbd0267dfef24b864c0f5
 
 const user = {
+    // get user data
+    async getUserInfo(req, res, next){
+        const data = await User.find();
+        successHandle(res, data);
+    },
+    // add user
+    async addUserInfo (req, res, next){
+        let {name , email, photo, password} = req.body;
+        const emailCheck = await User.findOne({"email" : email});
+        console.log(emailCheck);
+        if(emailCheck)
+            return next(customiError("400", "email duplicate", next));
 
-// get user data
-async getUserInfo(req, res, next){
-    const data = await User.find();
-    successHandle(res, data);
-},
+        if (!name || !email)
+            return next(customiError("400", "field error", next));
+        
+        if(!validator.isEmail(email))
+            return next(customiError("400", "Email form error", next));
+        
+        let newUser = await User.create({
+            "name" : name,
+            "email" : email,
+            "photo" : photo,
+            "password" : password
+        });
+        successHandle(res, newUser);
+    },
 
+    // edit user Info
+    async editUser(req, res, next){
+        let { name, email, id, photo} = req.body;
+        const editID = await User.findOne({ _id : id}).catch((error) => {console.log(123);});
+        console.log(editID)
+        if(editID == null){
+            return next(customiError("400", "id not found", next));
+        }  
+        if(!name || !email){
+            return next(customiError("400", "form error", next));
+        }
+        if(!validator.isEmail(email)){
+            return next(customiError("400", "email form error",next));
+        }
+        
+        let replaceData = await User.findOneAndReplace(id, {
+            "name" :  name,
+            "email" : email,
+            "id" : id,
+            "photo" : photo
+        })
+        successHandle(res, replaceData);
+    },
 
-// add user
-async addUserInfo (req, res, next){
-    let {name , email, photo, password} = req.body;
-    const emailCheck = await User.findOne({"email" : email});
-    if(emailCheck)
-        return customiError("400", "email duplicate", next);
-    
-    if (!name || !email)
-        return customiError("400", "field error", next);
-    
-    if(!validator.isEmail(email))
-        return customiError("400", "Email form error", next);
-    
-    let newUser = await User.create({
-        "name" : name,
-        "email" : email,
-        "photo" : photo,
-        "password" : password
-    });
-    successHandle(res, newUser);
-},
-
-// edit user Info
-async editUser(req, res, next){
-    let { name, email, id, photo} = req.body;
-    const editID = await User.findOne({ _id : id}).catch((error) => {console.log(123);});
-    console.log(editID)
-    if(editID == null){
-        return next(customiError("400", "id not found", next));
-    }  
-    if(!name || !email){
-        return next(customiError("400", "form error", next));
+    // delete user Info
+    async deleteUser(req, res, next){
+        let id = req.body.id
+        let userCheck = await User.findOne({
+            "_id" : id
+        })
+        if(!userCheck){
+            return next(customiError("400", "id not found", next));
+        }
+        let deleteUser = await User.findByIdAndRemove(id);
+        successHandle(res, deleteUser);
     }
-    if(!validator.isEmail(email)){
-        return next(customiError("400", "email form error",next));
-    }
-    
-    let replaceData = await User.findOneAndReplace(id, {
-        "name" :  name,
-        "email" : email,
-        "id" : id,
-        "photo" : photo
-    })
-    successHandle(res, replaceData);
-},
-
-// delete user Info
-async deleteUser(req, res, next){
-    let id = req.body.id
-    let userCheck = await User.findOne({
-        "_id" : id
-    })
-    if(!userCheck){
-        return next(customiError("400", "id not found", next));
-    }
-    let deleteUser = await User.findByIdAndRemove(id);
-    successHandle(res, deleteUser);
-},
-
-
 }
 
 
