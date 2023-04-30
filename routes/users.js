84b2bc11b49a659b444273ce59b1a3e1add4a5d9
AvocadoYang <cl4m3861026@gmail.com>
2023-04-30 13:07:52 +0800

    no message

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 let userRouter = require('../controllers/user');
 const handleErrorAsync = require('../errorHandle/handleErrorAsync');
+const jwtFn = require('../midleware/auth');
 var router = express.Router();
 
 
@@ -11,7 +12,7 @@ router.post('/logIn', handleErrorAsync(userRouter.logIn))
 
 router.post('/signUp', handleErrorAsync(userRouter.signUp));
 
-router.patch('/editInfo', handleErrorAsync(userRouter.editUserInfo));
+router.patch('/editInfo', jwtFn.isAuth, handleErrorAsync(userRouter.editUserInfo));
 
 router.delete('/', handleErrorAsync(userRouter.deleteUser));
 
